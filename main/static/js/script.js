1d9802e3e22d4070b6a9f116325279a35622b6de
tsmdev-ux <tiagosm.dev@gmail.com>
2025-06-24 17:55:55 -0300

    Preparando para deploy static08

--- main/static/js/script.js
+++ main/static/js/script.js
@@ -1,5 +1,8 @@
-
 function toggleMenu() {
-    const menu = document.getElementById("menu");
-    menu.classList.toggle("hidden");
+  const menu = document.getElementById("menu");
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+  } else {
+    menu.classList.add("hidden");
+  }
 }
