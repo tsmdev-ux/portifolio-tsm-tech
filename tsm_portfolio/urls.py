513d5dac0130a38733551768dcf8ba92ace65557
tsmdev-ux <tiagosm.dev@gmail.com>
2025-06-26 12:27:32 -0300

    Preparando para deploy static11

--- tsm_portfolio/urls.py
+++ tsm_portfolio/urls.py
@@ -1,16 +1,18 @@
-
 from django.contrib import admin
 from django.urls import path
 from main import views
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('', views.home, name='home'),
-     path('projetos/', views.projetos, name='projetos'),
-     path('sobre/', views.sobre, name='sobre'),
-     path('contato/', views.contato, name='contato'),
-
-
-
+    path('', views.home, name='home'),
+    path('projetos/', views.projetos, name='projetos'),
+    path('sobre/', views.sobre, name='sobre'),
+    path('contato/', views.contato, name='contato'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
